e4c5a504d91276a4ce9550017d71a6b3c17df056
fatur8-bit <78153603+fatur8-bit@users.noreply.github.com>
2021-02-02 19:20:05 +0700

    Update main.js

--- main.js
+++ main.js
@@ -12,9 +12,9 @@ let util = require('util')
 let WAConnection = simple.WAConnection(_WAConnection)
 
 
-global.owner = ['6281515860089','6281357302007','6288235435804'] // Put your number here
-global.mods = ['6281357302007','6288235435804'] // Want some help?
-global.prems = ['6281357302007','6288235435804'] // Premium user has unlimited limit
+global.owner = ['62859140514162','6282134037319'] // Put your number here
+global.mods = ['62859140514162','6282134037319'] // Want some help?
+global.prems = ['62859140514162','6282134037319'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
